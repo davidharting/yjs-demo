f24a74f87dd6fd9496ae62ee692bc53f61e1b8e6
David Harting <david.harting@hey.com>
2023-08-20 19:53:32 -0400

    add markdown preview

--- browser/src/App.tsx
+++ browser/src/App.tsx
@@ -1,14 +1,17 @@
 import Editor from "@monaco-editor/react";
+import { useSyncedStore } from "@syncedstore/react";
 import type { editor } from "monaco-editor";
+import ReactMarkdown from "react-markdown";
 import y from "yjs";
 
-import { useSyncedStore } from "@syncedstore/react";
 import { store } from "./store";
 
 const applyMonacoChangeToStore = (
   event: editor.IModelContentChangedEvent,
   doc: y.Text
 ): void => {
+  // TODO: Handle Undo
+  // TODO: Handle Redo
   event.changes.forEach((change) => {
     if (change.text === "") {
       doc.delete(change.rangeOffset, change.rangeLength);
@@ -28,12 +31,16 @@ function App() {
       <div className="editor-container">
         <Editor
           language="markdown"
-          height="90vh"
+          height="50vh"
           value={state.activeEditor.toString()}
           onChange={(_value, event) => {
             applyMonacoChangeToStore(event, state.activeEditor);
           }}
         />
+      </div>
+
+      <div style={{ height: "50vh" }}>
+        <ReactMarkdown>{state.activeEditor.toString()}</ReactMarkdown>
       </div>
     </main>
   );
