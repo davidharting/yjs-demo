589df5a47f1b02feeda30b6dea6742704172e167
David Harting <david.harting@hey.com>
2023-08-20 17:47:00 -0400

    Add indexed-db support

--- browser/src/store.ts
+++ browser/src/store.ts
@@ -1,3 +1,18 @@
-import { syncedStore } from "@syncedstore/core";
+import { syncedStore, getYjsDoc } from "@syncedstore/core";
+import { IndexeddbPersistence } from "y-indexeddb";
 
 export const store = syncedStore({ activeEditor: "text" });
+
+/**
+ * Documents need a name when using integrations.
+ * This way, the doc can be uniquely identified, in this case within IndexedDB and by the websocket server.
+ */
+const DOC_NAME = "yjsDemoState";
+
+/**
+ * SyncedStore is built directly on top of ydoc. ydoc integrations are not abstracted away from us.
+ * We can use the ydoc instance directly to setup local persistence and websocket integration.
+ */
+const ydoc = getYjsDoc(store);
+
+new IndexeddbPersistence(DOC_NAME, ydoc);
