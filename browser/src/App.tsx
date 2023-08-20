0ecdf2f0867d5a616e2cd8671f42f529099b403c
David Harting <david.harting@hey.com>
2023-08-20 17:28:14 -0400

    sync text

--- browser/src/App.tsx
+++ browser/src/App.tsx
@@ -2,6 +2,16 @@ function App() {
   return (
     <main>
       <h1>y-js demo</h1>
+
+      <form>
+        <div>
+          <label htmlFor="textEditor">
+            Add text here
+            <textarea id="textEditor" rows={6} />
+            <small>It will be synced automatically</small>
+          </label>
+        </div>
+      </form>
     </main>
   );
 }
