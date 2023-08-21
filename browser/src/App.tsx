83cca1e3b0fa79f75e0a72467665b47703715f6e
David Harting <david.harting@hey.com>
2023-08-20 20:24:42 -0400

    Add Monaco (#1)

--- browser/src/App.tsx
+++ browser/src/App.tsx
@@ -1,35 +1,47 @@
+import Editor from "@monaco-editor/react";
 import { useSyncedStore } from "@syncedstore/react";
+import type { editor } from "monaco-editor";
+import ReactMarkdown from "react-markdown";
+import y from "yjs";
+
 import { store } from "./store";
+
+const applyMonacoChangeToStore = (
+  event: editor.IModelContentChangedEvent,
+  doc: y.Text
+): void => {
+  // TODO: Handle Undo
+  // TODO: Handle Redo
+  event.changes.forEach((change) => {
+    if (change.text === "") {
+      doc.delete(change.rangeOffset, change.rangeLength);
+    } else {
+      doc.insert(change.rangeOffset, change.text);
+    }
+  });
+};
 
 function App() {
   const state = useSyncedStore(store);
-
-  const overwrite = (text: string) => {
-    state.activeEditor.delete(0, state.activeEditor.length);
-    state.activeEditor.insert(0, text);
-  };
 
   return (
     <main>
       <h1>y-js demo</h1>
 
-      <form>
-        <div>
-          <label htmlFor="textEditor">
-            Add text here
-            <textarea
-              id="textEditor"
-              rows={6}
-              value={state.activeEditor.toString()}
-              onChange={(e) => {
-                if (typeof e.target.value === "string")
-                  overwrite(e.target.value);
-              }}
-            />
-            <small>It will be synced automatically</small>
-          </label>
-        </div>
-      </form>
+      <div className="editor-container">
+        <Editor
+          language="markdown"
+          height="50vh"
+          value={state.activeEditor.toString()}
+          onChange={(_value, event) => {
+            applyMonacoChangeToStore(event, state.activeEditor);
+          }}
+        />
+      </div>
+
+      <div style={{ height: "50vh" }}>
+        <ReactMarkdown>{state.activeEditor.toString()}</ReactMarkdown>
+      </div>
     </main>
   );
 }
